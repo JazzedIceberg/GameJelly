3b511c79fea8f5a733d30011ed7e7529d9b4f5bc
JazzedIceberg <58668956+SamDoesStuff1@users.noreply.github.com>
2022-01-08 17:00:43 +0000

    3rd times the charm

---
+++ Jelly/Assets/Scripts/ShowResource.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShowResource : MonoBehaviour
+{
+    public Sprite icon;
+    public int quantity;
+
+    private TextMeshProUGUI num;
+    private Image mini;
+
+    void Start()
+    {
+        mini = gameObject.GetComponentInChildren<Image>();
+        mini.sprite = icon;
+
+        num = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        num.text = quantity.ToString();
+    }
+}
